0da90ae260c92874d2db3a2917094e213efaef45
lukqis <lukiqs@gmail.com>
2017-11-09 00:54:04 +0100

    dziedziczenie

---
+++ php_zaawansowane_programowanie_macintyre/dziedziczenie_2.php
@@ -0,0 +1,56 @@
+<?php
+
+abstract class Figure{
+    abstract public function getField();
+    abstract public function getCircuit();
+}
+
+class Circle extends Figure{
+    
+    public function getCircuit() {
+        
+    }
+    
+    public function getField() {
+        
+    }
+    
+}
+
+class Triangle extends Figure{
+    
+    public function getCircuit() {
+        
+    }
+    
+    public function getField() {
+        
+    }
+        
+}
+
+class Rectangle extends Figure{
+    
+    public function getCircuit() {
+        
+    }
+    
+    public function getField() {
+        
+    }
+    
+}
+
+class Polygon extends Figure{
+    
+    public function getCircuit() {
+        
+    }
+
+    public function getField() {
+        
+    }
+
+}
+
+// ect
